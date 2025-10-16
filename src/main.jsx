3755ec60ef77822c5904e17e7768160991c657a8
aliseena.a <aliseena.ahmar@ontariotechu.net>
2025-10-16 19:46:14 -0400

    added favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import portfolioIcon from './assets/portfolio.png';
+
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/png'
+link.href = portfolioIcon
+document.head.appendChild(link)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
